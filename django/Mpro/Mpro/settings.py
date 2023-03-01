0f7f2ddea26e8f8c719f9e2d229b0b096f648bbd
yu-je0ng <106722166+yu-je0ng@users.noreply.github.com>
2023-03-02 01:55:11 +0900

    Fixed secret key

--- django/Mpro/Mpro/settings.py
+++ django/Mpro/Mpro/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-8lyt()*iqtzc(4e+4h&&kmot6n*hb4q%9vzr6(_q!8v9)tpkik'
+SECRET_KEY = 'secret key'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
